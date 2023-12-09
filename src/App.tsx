601b86fd878429ee187443fb421eb6d204c5ae2e
Umair Jameel <umairjameel321@gmail.com>
2023-12-10 01:01:36 +0500

    change in text.

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more about project
       </p>
     </>
   );
